807793747a34653321a14f4855e473b5f4d303db
Laky64 <iraci.matteo@gmail.com>
2024-08-30 21:25:40 +0200

    Fixed Layer Number Extraction

--- android/extract_layer.go
+++ android/extract_layer.go
@@ -10,10 +10,10 @@ import (
 )
 
 func ExtractLayer() (int, error) {
-	readFile, err := os.ReadFile(path.Join(environment.EnvFolder, consts.TempSources, "ConnectionsManager.java"))
+	readFile, err := os.ReadFile(path.Join(environment.EnvFolder, consts.TempSources, "TLRPC$Message.java"))
 	if err != nil {
 		return -1, err
 	}
-	layer := regexp.MustCompile(`init\(.*?, ([0-9]+),`).FindAllStringSubmatch(string(readFile), -1)[0][1]
+	layer := regexp.MustCompile(`this.layer = ([0-9]+);`).FindAllStringSubmatch(string(readFile), -1)[0][1]
 	return strconv.Atoi(layer)
 }
