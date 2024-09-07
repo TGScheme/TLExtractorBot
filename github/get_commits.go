a177ddf6b61a5e104462dba597a6b313895f22d6
Laky64 <iraci.matteo@gmail.com>
2024-09-07 14:46:57 +0200

    Added "GetCommits" to Github Client

---
+++ github/get_commits.go
@@ -0,0 +1,13 @@
+package github
+
+import "github.com/google/go-github/v62/github"
+
+func (ctx *clientContext) GetCommits(repoOwner, repoName, path string) ([]*github.RepositoryCommit, error) {
+	commits, resp, err := ctx.client.Repositories.ListCommits(ctx.ctx, repoOwner, repoName, &github.CommitsListOptions{
+		Path: path,
+	})
+	if err != nil && (resp == nil || resp.StatusCode != 404) {
+		return nil, err
+	}
+	return commits, nil
+}
