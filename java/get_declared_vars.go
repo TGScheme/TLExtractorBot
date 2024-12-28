8c3f0804bb3c6bc740d29ecfdecdaaf99cd7b16f
Laky64 <iraci.matteo@gmail.com>
2024-12-28 21:01:14 +0100

    Fixed parent vars declaration order

--- java/get_declared_vars.go
+++ java/get_declared_vars.go
@@ -4,14 +4,14 @@ import javaTypes "TLExtractor/java/types"
 
 func getDeclaredVars(class *javaTypes.RawClass) map[string]string {
 	declaredVars := make(map[string]string)
-	for _, line := range class.Content {
-		if res := GetVarDeclaration(line); res != nil {
-			declaredVars[res.Name] = res.Type
-		}
-	}
 	if class.ParentLink != nil {
 		for key, value := range getDeclaredVars(class.ParentLink) {
 			declaredVars[key] = value
+		}
+	}
+	for _, line := range class.Content {
+		if res := GetVarDeclaration(line); res != nil {
+			declaredVars[res.Name] = res.Type
 		}
 	}
 	return declaredVars
